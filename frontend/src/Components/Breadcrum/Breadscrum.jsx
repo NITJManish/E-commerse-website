15b7161c69ed00a4d9a92da80e3b27a6e65dbd99
neera <kumarmanish150799@gmail.com>
2024-02-20 06:43:10 +0530

    frontend part

---
+++ frontend/src/Components/Breadcrum/Breadscrum.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Breadscrum.css'
+import arrow_icon from '../Assestsbreadcrum_arrow.png';
+
+const Breadscrum = (props) => {
+    const {product}=props;
+  return (
+    <div className='breadscrum'>
+    Home <img src={arrow_icon} alt="" />SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
+    </div>
+  )
+}
+
+export default Breadscrum
